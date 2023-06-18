f22e40d2a872915e4fbbbff184253b710eeeb1d8
roy.blows59@gmail.com <roy.blows59@gmail.com>
2023-06-18 22:33:18 +0100

    New version 6.3

--- M3da/DBase.cpp
+++ M3da/DBase.cpp
@@ -13820,7 +13820,7 @@ if (this->pCurrentMesh!=NULL)
     {
     fprintf(pFile2,"%s\n","$**********************************************************");
     fprintf(pFile2,"%s\n","$      NASTRAN DECK EXPORTED FROM M3D");
-    fprintf(pFile2,"%s\n","$      VERSION 6.1");
+    fprintf(pFile2,"%s\n","$      VERSION 6.3");
     fprintf(pFile2,"%s\n","$      www.M3dFea.com");
     fprintf(pFile2,"%s %i:%i:%i\n","$      DATE",Day,Mon,Year);
     fprintf(pFile2,"%s %i:%i:%i\n","$      TIME",Hour,Min,Sec);
